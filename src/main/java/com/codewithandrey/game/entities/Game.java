f4f87b9af9421dc2ef12f13a1075c130f75c891c
Andrey Oliveira <dedey.cardoso@gmail.com>
2025-05-19 22:32:13 -0300

    Table Game, application properties, h2 Database

---
+++ src/main/java/com/codewithandrey/game/entities/Game.java
@@ -0,0 +1,26 @@
+package com.codewithandrey.game.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private Double score;
+    private String plataform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
